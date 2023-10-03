bd6104a727c1e73b6dd090e016192b5cdec41d2b
ChahturaMathula <chathuraekanayake927@hotmail.com>
2023-10-03 20:07:40 +0530

    refactored apiHelper function

--- src/helpers/apiHelpers.js
+++ src/helpers/apiHelpers.js
@@ -3,23 +3,27 @@ import { WEATHER_API_BASE_URL, WEATHER_API_KEY } from "../constants/apiConstants
 
 const fetchWeatherDataByCityCodes = async (cityCodes) => {
 
-    try {
-        const cityCodesString = cityCodes.join(",");
+    if (cityCodes?.length > 0) {
+        try {
+            const cityCodesString = cityCodes.join();
+            const response = await fetch(
+                `${WEATHER_API_BASE_URL}?id=${cityCodesString}&units=metric&appid=${WEATHER_API_KEY}`
+            );
 
-        const response = await fetch(
-            `${WEATHER_API_BASE_URL}?id=${cityCodesString}&units=metric&appid=${WEATHER_API_KEY}`
-        );
 
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
+            if (!response.ok) {
+                console.log("====>", response)
+                throw new Error(`HTTP error! status: ${response.status} (${response.statusText})`);
+            }
+
+            const weatherData = response.json();
+            return weatherData;
+
+
+        } catch (error) {
+            console.log("Error fetching weather data", error);
+            throw error;
         }
-
-        const weatherData = response.json();
-        return weatherData;
-
-    } catch (error) {
-        console.log("Error fetching weather data", error);
-        throw error;
     }
 
 };
