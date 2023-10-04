48b75cb899cb66a4166cd0492b338b69689ffd39
ChahturaMathula <chathuraekanayake927@hotmail.com>
2023-10-04 19:27:57 +0530

    refactored

--- src/constants/apiConstants.js
+++ src/constants/apiConstants.js
@@ -1,5 +1,5 @@
 
-export const WEATHER_API_KEY = "f05f817cf9e3ab4d6b6b5f2c8152a844";
+export const WEATHER_API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 
 export const WEATHER_API_BASE_URL = "https://api.openweathermap.org/data/2.5/group";
 
