32320248e64db1192fe8fe4eb35070e99a1a0df8
ChahturaMathula <chathuraekanayake927@hotmail.com>
2023-10-25 15:17:40 +0530

    main.jsx refactored

--- src/jsx/main.jsx
+++ src/jsx/main.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '../css/index.css';
 import WeatherApp from './components/WeatherApp.jsx';
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
